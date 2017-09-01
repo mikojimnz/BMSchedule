052f67abd7c68718875a5b2c6427bc301d9d51ca
mikojimnz <jimenezmikog@gmail.com>
2017-09-01 13:41:11 -0400

    Attempt to fix highlighting

--- Scripts/main.js
+++ Scripts/main.js
@@ -26,7 +26,7 @@ function Update() {
             if (hours == 12 && mins >= 13 && mins <= 58) return "h6";
             if (hours == 13 && mins >= 4 && mins <= 49) return "h7";
             if ((hours == 13 && mins >= 55 && mins <= 59) || (hours == 14 && mins >= 0 && mins <= 40)) return "h8";
-        } else if (daysShort == "wed") {
+        } else if (dayShort == "wed") {
             if (hours == 7 && mins >= 40 && mins <= 52) return "hr";
         } else if (dayShort == "wed"){
             if (hours == 7 && mins >= 40 && mins <= 52) return "hr"; 
@@ -47,6 +47,7 @@ function Update() {
 
     if (Hour() != null) {
         var get = document.getElementById(Day).getElementsByClassName(ClassHour())[0].style;
+    }
     
     if (ClassHour() != null) {
         var get = document.getElementById(dayShort).getElementsByClassName(ClassHour())[0].style;
