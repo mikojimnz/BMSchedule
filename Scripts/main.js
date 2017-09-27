8036f92f38720844f2ed51bd4561cbe698d086d2
Miko <mikojimnz@users.noreply.github.com>
2017-09-27 10:26:33 -0400

    Fixed Wednesday from git merge error

--- Scripts/main.js
+++ Scripts/main.js
@@ -26,8 +26,6 @@ function Update() {
             if (hours == 12 && mins >= 13 && mins <= 58) return "h6";
             if (hours == 13 && mins >= 4 && mins <= 49) return "h7";
             if ((hours == 13 && mins >= 55 && mins <= 59) || (hours == 14 && mins >= 0 && mins <= 40)) return "h8";
-        } else if (dayShort == "wed") {
-            if (hours == 7 && mins >= 40 && mins <= 52) return "hr";
         } else if (dayShort == "wed"){
             if (hours == 7 && mins >= 40 && mins <= 52) return "hr"; 
             if ((hours == 7 && mins >= 58 && mins <= 59) || (hours == 8 && mins >= 0 && mins <= 39)) return "h1";
